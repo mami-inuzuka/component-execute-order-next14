e67da83cfb984c31ba8f3d44abf80f744719c15e
mami-inuzuka <inuzuka.mami@gmail.com>
2024-12-01 14:47:48 +0900

    layout.js と page.js を AsyncComponent にする

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
+  console.log("layout");
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("layout after 1 sec");
+
   return (
     <html lang="en">
       <body>{children}</body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
+export default async function Home() {
+  console.log("page");
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("page after 1 sec");
+
   return <>HOME</>;
 }
